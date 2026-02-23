2f62330da089d2926dc7ac977276ae7e85a34fdc
Ayushmishra8605 <mishraayush7287@gmail.com>
2026-02-23 13:47:44 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import google.generativeai as genai
+
+def run_chatbot(api_key):
+    # Gemini Setup
+    genai.configure(api_key=(api_key))
+    model = genai.GenerativeModel('gemini-pro')
+
+    st.divider()
+    st.subheader("Urmila Traders AI Assistant")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Chat history dikhane ke liye
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # User Input aur AI Response
+    if prompt := st.chat_input("Ask me anything..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            try:
+                response = model.start_chat().send_message(prompt)
+                st.markdown(response.text)
+                st.session_state.messages.append({"role": "assistant", "content": response.text})
+            except Exception as e:
+                st.error(f"Error: {e}")
