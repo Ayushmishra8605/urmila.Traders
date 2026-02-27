e7ad2e9886e96f823eb02f358542838e2a6f72e8
Ayushmishra8605 <mishraayush7287@gmail.com>
2026-02-27 07:41:03 +0530

    Add files via upload

---
+++ style.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+def apply_custom_design():
+    # Page Title aur Layout set karein
+    st.set_page_config(page_title="Urmila Traders", layout="centered")
+
+    # Streamlit ka header aur footer chhupane ke liye CSS
+    hide_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_style, unsafe_allow_html=True)
+
+
+def show_sidebar_content(phone_number):
+    # Sidebar mein logo (bina gol kiye)
+    # Check karein ki GitHub par image ka sahi naam yahi hai
+   # st.sidebar.image("urmila traders.png", width=200)
+
+    # Clickable phone number (Blue link)
+    st.sidebar.markdown(f"### Contact Us: [+{phone_number}](tel:{phone_number})")
